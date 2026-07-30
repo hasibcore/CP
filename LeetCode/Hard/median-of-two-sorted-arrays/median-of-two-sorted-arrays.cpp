8c89a145245d83163ea19dc95923c4d035da106f
hasibcore <hasibcore@users.noreply.github.com>
2026-07-30 20:52:29 +0600

    Time: 0ms | Memory: 0MB | Language: cpp

---
+++ LeetCode/Hard/median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,12 @@
+            nums.push_back(nums2[j++]);
+
+        int n = nums.size();
+
+        if (n % 2 == 1)
+            return nums[n / 2];
+
+        return (nums[n / 2 - 1] + nums[n / 2]) / 2.0;
+    }
+};
+
+
